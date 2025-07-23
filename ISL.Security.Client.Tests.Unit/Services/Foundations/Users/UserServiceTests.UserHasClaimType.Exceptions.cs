b60b286a39760966b49a9bfc9ba3fb6aabef8c11
Christo du Toit <christo@dutoit.co.uk>
2025-07-23 13:53:44 +0100

    ShouldThrowServiceExceptionOnUserHasClaimTypeIfServiceErrorOccursAndLogItAsync -> PASS

--- ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.UserHasClaimType.Exceptions.cs
+++ ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.UserHasClaimType.Exceptions.cs
@@ -2,10 +2,59 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Security.Client.Models.Foundations.Users.Exceptions;
+using ISL.Security.Client.Services.Foundations.Users;
+using Moq;
+
 namespace ISL.Security.Client.Tests.Unit.Services.Foundations.Users
 {
     public partial class UserServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnUserHasClaimTypeIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            ClaimsPrincipal someClaimsPrincipal = new ClaimsPrincipal();
+            string someClaimType = GetRandomString();
+            var serviceException = new Exception();
 
+            var failedUserServiceException =
+                new FailedUserServiceException(
+                    message: "Failed user service error occurred, please contact support.",
+                    innerException: serviceException);
+
+            var expectedUserServiceException =
+                new UserServiceException(
+                    message: "User service error occurred, please contact support.",
+                    innerException: failedUserServiceException);
+
+            var userServiceMock = new Mock<UserService> { CallBase = true };
+
+            userServiceMock.Setup(broker =>
+                broker.ValidateOnUserHasClaimType(It.IsAny<ClaimsPrincipal>(), It.IsAny<string>()))
+                    .Throws(serviceException);
+
+            // when
+            ValueTask<bool> userHasClaimTypeTask =
+                userServiceMock.Object.UserHasClaimTypeAsync(someClaimsPrincipal, someClaimType);
+
+            UserServiceException actualUserServiceException =
+                await Assert.ThrowsAsync<UserServiceException>(
+                    userHasClaimTypeTask.AsTask);
+
+            // then
+            actualUserServiceException.Should()
+                .BeEquivalentTo(expectedUserServiceException);
+
+            userServiceMock.Verify(broker =>
+                broker.ValidateOnUserHasClaimType(It.IsAny<ClaimsPrincipal>(), It.IsAny<string>()),
+                    Times.Once);
+
+            userServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
