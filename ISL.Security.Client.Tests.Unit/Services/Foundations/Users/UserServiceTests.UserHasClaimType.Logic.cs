56f25576f6a0e3256ac2597ee3ebbc3d3b924102
Christo du Toit <christo@dutoit.co.uk>
2025-07-23 13:40:27 +0100

    ShouldPerformUserHasClaimTypeAsync -> PASS

--- ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.UserHasClaimType.Logic.cs
+++ ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.UserHasClaimType.Logic.cs
@@ -2,10 +2,42 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Security.Client.Models.Foundations.Users;
+
 namespace ISL.Security.Client.Tests.Unit.Services.Foundations.Users
 {
     public partial class UserServiceTests
     {
+        [Theory]
+        [InlineData("displayName", true)]
+        [InlineData("nickName", false)]
+        public async Task ShouldPerformUserHasClaimTypeAsync(string claimType, bool hasClaimType)
+        {
+            // Given
+            ClaimsPrincipal claimsPrincipal = CreateRandomClaimsPrincipal();
+            bool expectedResult = hasClaimType;
+
+            User expectedUser = new User(
+                userId: claimsPrincipal.FindFirst("oid")?.Value,
+                givenName: claimsPrincipal.FindFirst(ClaimTypes.GivenName)?.Value,
+                surname: claimsPrincipal.FindFirst(ClaimTypes.Surname)?.Value,
+                displayName: claimsPrincipal.FindFirst("displayName")?.Value,
+                email: claimsPrincipal.FindFirst(ClaimTypes.Email)?.Value,
+                jobTitle: claimsPrincipal.FindFirst("jobTitle")?.Value,
+                roles: claimsPrincipal.FindAll(ClaimTypes.Role).Select(role => role.Value).ToList(),
+                claims: claimsPrincipal.Claims.ToList());
+
+            // When
+            bool actualResult = await this.userService.UserHasClaimTypeAsync(claimsPrincipal, claimType);
+
+            // Then
+            actualResult.Should().Be(expectedResult);
+        }
+
 
     }
 }
