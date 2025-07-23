b9b17bcb6c99ce7c2ba61d6de00dacfff5919e50
Christo du Toit <christo@dutoit.co.uk>
2025-07-23 12:13:07 +0100

    ShouldPerformIsUserInRoleAsync -> PASS

--- ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.IsUserInRole.Logic.cs
+++ ISL.Security.Client.Tests.Unit/Services/Foundations/Users/UserServiceTests.IsUserInRole.Logic.cs
@@ -2,10 +2,40 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Security.Client.Models.Foundations.Users;
+
 namespace ISL.Security.Client.Tests.Unit.Services.Foundations.Users
 {
     public partial class UserServiceTests
     {
+        [Theory]
+        [InlineData("Users", true)]
+        [InlineData("SuperUsers", false)]
+        public async Task ShouldPerformIsUserInRoleAsync(string roleName, bool isInRole)
+        {
+            // Given
+            ClaimsPrincipal claimsPrincipal = CreateRandomClaimsPrincipal();
+            bool expectedResult = isInRole;
 
+            User expectedUser = new User(
+                userId: claimsPrincipal.FindFirst("oid")?.Value,
+                givenName: claimsPrincipal.FindFirst(ClaimTypes.GivenName)?.Value,
+                surname: claimsPrincipal.FindFirst(ClaimTypes.Surname)?.Value,
+                displayName: claimsPrincipal.FindFirst("displayName")?.Value,
+                email: claimsPrincipal.FindFirst(ClaimTypes.Email)?.Value,
+                jobTitle: claimsPrincipal.FindFirst("jobTitle")?.Value,
+                roles: claimsPrincipal.FindAll(ClaimTypes.Role).Select(role => role.Value).ToList(),
+                claims: claimsPrincipal.Claims.ToList());
+
+            // When
+            bool actualResult = await this.userService.IsUserInRoleAsync(claimsPrincipal, roleName);
+
+            // Then
+            actualResult.Should().Be(expectedResult);
+        }
     }
 }
