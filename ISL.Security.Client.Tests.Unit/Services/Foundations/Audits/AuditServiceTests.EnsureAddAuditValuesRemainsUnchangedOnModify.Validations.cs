41e88d1c86e9edf869af5fd67b244aa35f6fa936
Christo du Toit <christo@dutoit.co.uk>
2025-07-28 15:30:23 +0100

    ShouldThrowValidationExceptionOnEnsureAddAuditValuesIfNullsFoundAsync -> PASS

--- ISL.Security.Client.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.EnsureAddAuditValuesRemainsUnchangedOnModify.Validations.cs
+++ ISL.Security.Client.Tests.Unit/Services/Foundations/Audits/AuditServiceTests.EnsureAddAuditValuesRemainsUnchangedOnModify.Validations.cs
@@ -2,10 +2,57 @@
 // Copyright (c) North East London ICB. All rights reserved.
 // ---------------------------------------------------------
 
+using System.Threading.Tasks;
+using FluentAssertions;
+using ISL.Security.Client.Models.Clients;
+using ISL.Security.Client.Models.Foundations.Audits.Exceptions;
+using ISL.Security.Client.Tests.Unit.Models;
+
 namespace ISL.Security.Client.Tests.Unit.Services.Foundations.Audits
 {
     public partial class AuditServiceTests
     {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnEnsureAddAuditValuesIfNullsFoundAsync()
+        {
+            // given
+            Person nullInputPerson = null;
+            Person nullStoragePerson = null;
+            SecurityConfigurations nullSecurityConfigurations = null;
 
+            InvalidArgumentAuditException invalidArgumentAuditException = new InvalidArgumentAuditException(
+                message: "Invalid audit argument(s), correct the errors and try again.");
+
+            invalidArgumentAuditException.AddData(
+                key: "entity",
+                values: "Entity is required");
+
+            invalidArgumentAuditException.AddData(
+                key: "storageEntity",
+                values: "Entity is required");
+
+            invalidArgumentAuditException.AddData(
+                key: nameof(SecurityConfigurations),
+                values: "Entity is required");
+
+            var expectedAuditValidationException =
+                new AuditValidationException(
+                    message: "Audit validation errors occurred, please try again.",
+                    innerException: invalidArgumentAuditException);
+
+            // when
+            ValueTask<Person> applyAddAuditTask =
+                auditService.EnsureAddAuditValuesRemainsUnchangedOnModifyAsync(
+                    nullInputPerson,
+                    nullStoragePerson,
+                    nullSecurityConfigurations);
+
+            AuditValidationException actualAuditValidationException =
+                await Assert.ThrowsAsync<AuditValidationException>(applyAddAuditTask.AsTask);
+
+            // then
+            actualAuditValidationException.Should()
+                .BeEquivalentTo(expectedAuditValidationException);
+        }
     }
 }
